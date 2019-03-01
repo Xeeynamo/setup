cd564b4cbeb692aafdf3388faa044b7e3235c46a
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-03-01 18:29:00 +0000

    Add download helpers

--- modules/common.psm1
+++ modules/common.psm1
@@ -1,8 +1,38 @@
+$tempDir = "./.tmp"
+
 function Get-TemporaryDownload([string]$uri) {
     $tmpFile = New-TemporaryFile
     Invoke-WebRequest -Uri $uri -OutFile $tmpFile.FullName
 
     return $tmpFile.FullName
+}
+
+function Unzip([string]$zipfile, [string]$outpath) {
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+
+function New-TempDirectory {
+    if (!(Test-Path -Path $tempDir)) {
+        New-Item -ItemType Directory -Force -Path $tempDir
+    }
+}
+
+function Remove-TempDirectory {
+    if (Test-Path -Path $tempDir) {
+        Remove-Item $tempDir -Recurse -Force
+    }
+}
+
+function Invoke-TemporaryZipDownload([string]$name, [string]$uri, $action) {
+    $outFile = Join-Path $tempDir ($name + ".zip")
+    $outDir = Join-Path $tempDir $name
+
+    Invoke-WebRequest -Uri $uri -OutFile $outFile
+    Unzip $outFile $outDir
+    Push-Location $outDir
+    $action
+    Pop-Location
 }
 
 function Install-UserProfile {
