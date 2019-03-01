b17676a048eabafff22c089ad0a7ae88a1c39671
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-03-01 17:25:59 +0000

    Add home directory set-up

--- modules/common.psm1
+++ modules/common.psm1
@@ -1,7 +1,12 @@
-Get-TemporaryDownload([string]$uri) {
+function Get-TemporaryDownload([string]$uri) {
     $tmpFile = New-TemporaryFile
     Invoke-WebRequest -Uri $uri -OutFile $tmpFile.FullName
 
     return $tmpFile.FullName
 }
 
+function Install-UserProfile {
+    Get-ChildItem -Path './home' |
+        Select-Object -ExpandProperty Name |
+        Copy-Item -Path $_ -Destination '~/'
+}
