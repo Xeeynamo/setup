3e9651fb335d042b3ec83a90380d7ee9a421d7ea
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-03-01 13:15:40 +0000

    Remove comments from chocopkg

--- setup.ps1
+++ setup.ps1
@@ -3,6 +3,7 @@ Write-Output "Beginning the set-up"
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 Get-Content 'chocopkg.txt' |
+    Where-Object { $_[0] -ne '#' -and $_.Length -gt 0} |
     ForEach-Object {
         & choco install $_ -y
     }
