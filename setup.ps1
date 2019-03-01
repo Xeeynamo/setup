4fca12482688beb220e12acfba11e2198d87ea69
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-03-01 13:00:55 +0000

    Add Google Chrome

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,10 @@
 Write-Output "Beginning the set-up"
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+@(
+    "googlechrome"
+) |
+    ForEach-Object {
+        & choco install $_ -y
+    }
