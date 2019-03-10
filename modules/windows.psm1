79f5292e63f05f77b3fab5fabe3b5c0ed68939fc
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-03-10 18:44:27 +0000

    Use of reg delete instead of Remove-Item

--- modules/windows.psm1
+++ modules/windows.psm1
@@ -2,7 +2,7 @@ function Install-StartLayout([string]$fileName) {
     Import-StartLayout -LayoutPath $fileName -MountPath $env:SystemDrive\
 
     # This will reset the start menu
-    Remove-Item -Path "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount"
+    reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount" /va /f
 
     # Restart explorer to apply the changes
     Stop-Process -ProcessName "explorer"
