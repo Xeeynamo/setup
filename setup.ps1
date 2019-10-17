18dd6aefed050649750a63c8c4e47cfc70ac7069
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-10-17 22:15:22 +0100

    Remove unused script

--- setup.ps1
+++
@@ -1,8 +0,0 @@
-Write-Output "Beginning the set-up"
-Get-ChildItem .\modules\*.psm1 | Import-Module -Force
-
-Invoke-TemporaryGitDownload "debloat" "https://github.com/W4RH4WK/Debloat-Windows-10" {
-    & ./scripts/block-telemetry.ps1
-}
-
-Remove-TempDirectory
