42c3c6c10c4d6ed2db06b5733876cd4631fd7f9d
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2019-11-14 16:20:11 +0000

    Avoid to accidentally delete the entire disk

--- linux/pre.sh
+++ linux/pre.sh
@@ -1,12 +1,16 @@
+if [ -z $DISK ]; then printf "\e[1;31mVariable DISK is not set (eg. DISK=/dev/sda). Setup process will now exit."; fi
+DISK1="$DISK""1"
+DISK2="$DISK""2"
+
 #ip link
 loadkeys uk
 timedatectl set-ntp true
-printf "g\nn\n\n\n+100M\nn\n\n\n\nw\n" | fdisk /dev/sda
-mkfs.ext4 /dev/sda2
-mkfs.fat -F32 /dev/sda1
-mount /dev/sda2 /mnt
+printf "g\nn\n\n\n+100M\nn\n\n\n\nw\n" | fdisk $DISK
+mkfs.ext4 $DISK2
+mkfs.fat -F32 $DISK1
+mount $DISK2 /mnt
 mkdir /mnt/efi
-mount /dev/sda1 /mnt/efi
+mount $DISK1 /mnt/efi
 pacstrap /mnt base linux linux-firmware grub efibootmgr base-devel git nano dhcpcd openssh systemd ufw wget curl
 genfstab -U /mnt >> /mnt/etc/fstab
 curl https://raw.githubusercontent.com/Xeeynamo/setup/master/linux/setup.sh | arch-chroot /mnt
